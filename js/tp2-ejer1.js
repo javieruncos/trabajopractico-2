bab1540213afe283843be1af6988edf934beea31
Javier Uncos <javiersrfc30@gmail.com>
2022-07-08 14:10:59 -0300

    comenzamos a reazalir los ejercicios ,numero 2 realizado

---
+++ js/tp2-ejer1.js
@@ -0,0 +1,6 @@
+let edad = parseInt(prompt("ingresa tu edad"));
+if(edad >= 18){
+    document.write("Estas habilitado para conducir")
+}else{
+    document.write("No estas habilitado para conducir")
+}
---
+++ js/tp2-ejer2.js
@@ -0,0 +1,40 @@
+
+let nota = parseInt(prompt("ingresa un numero"));
+if(nota <= 0 || nota <= 2 ){
+     alert("muy deficiente");
+ }else if(nota <= 3 || nota <= 4){
+        alert("Insuficiente");
+}else if (nota <= 5 || nota <= 6 ){
+        alert("Suficiente")
+}else if (nota <=7){
+        alert("Bien");
+} else if(nota <=8  || nota <=9){
+        alert("notable");
+}else if(nota <= 10){
+        alert("sobresaliente");
+}else{
+    document.write("ingresa un numero valido")
+    alert("numero erroneo")
+}
+
+// let nota = parseInt(prompt("ingresa un numero"));
+// while(nota <= 10){
+//     // let nota = parseInt(prompt("ingresa un numero"));
+// if(nota <= 0 || nota <= 2 ){
+//      alert("muy deficiente");
+//  }else if(nota <= 3 || nota <= 4){
+//         alert("Insuficiente");
+// }else if (nota <= 5 || nota <= 6 ){
+//         alert("Suficiente")
+// }else if (nota <=7){
+//         alert("Bien");
+// } else if(nota <=8  || nota <=9){
+//         alert("notable");
+// }else if(nota <= 10){
+//         alert("sobresaliente");
+// }else{
+//     alert("numero erroneo");
+//     document.write("ingresa un numero valido" )  
+// }
+    
+// }
