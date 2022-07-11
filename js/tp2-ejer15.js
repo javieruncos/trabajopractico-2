a85e5ab5656c6df1fea522a936cf7e43ff2790f3
Javier Uncos <javiersrfc30@gmail.com>
2022-07-11 13:55:08 -0300

    realizamos el ejercicio 14 y 15, hay algunas dudas sobre el contadorde vocales

--- js/tp2-ejer15.js
+++ js/tp2-ejer15.js
@@ -1,8 +1,21 @@
-// let cantidadmates = 0;
-// while(cantidadmates<=20){
-//     console.log("cantidad de mates cebados "+ cantidadmates)
-//     cantidadmates++
+
+
+let frase = prompt("ingresa una frase");
+frase = frase.toLowerCase()
+let vocal = ["a","e","i","o","u"];
+let contador = 0;
+
+for(let i = 0; i<frase.length;i++){
+    if (vocal.indexOf(frase[i])
+    != -1){
+        contador++
+        console.log(frase);
+        
+    }
+}
+//     if(frase.charAt(i)=== vocal[0]||frase.charAt(i)=== vocal[1]||frase.charAt(i)=== vocal[3]||frase.charAt(i)=== vocal[4]||frase.charAt(i)=== vocal[5]){
+//         contador++
+//         console.log(contador)
+//     }
 // }
-
-
 
---
+++ js/tp2-ejer16.js
@@ -0,0 +1,6 @@
+let frase = prompt("ingressa una frase");
+for(let i = frase.length - 1; i>=0 ;i--){
+    document.write(frase.charAt(i))
+}
+
+
