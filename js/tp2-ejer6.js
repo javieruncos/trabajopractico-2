15e6dc8ab7430ca119e3c87ec2c88e6a793a384c
Javier Uncos <javiersrfc30@gmail.com>
2022-07-12 19:19:43 -0300

    realizamos el ejercico 7

--- js/tp2-ejer6.js
+++ js/tp2-ejer6.js
@@ -1,12 +1,11 @@
 
 
 let numero1 = prompt("ingresa un numero");
-// let numero2 = parseInt(prompt("ingresa un numero"));
-// let numero3 = parseInt(prompt("ingresa un numero"));
-// let numero4 = parseInt(prompt("ingresa un numero"));
-// let numero5 = parseInt(prompt("ingresa un numero"));
+
 
 
 for (let i = 0;i <= numero1;i++){
     console.log(numero1.repeat(i))
 }
+
+
---
+++ js/tp2-ejer7.js
@@ -0,0 +1,10 @@
+
+
+let numero1 = prompt("ingresa un numero");
+
+
+
+for (let i = 10;i >= numero1;i--){
+    console.log(numero1.repeat(i))
+}
+
