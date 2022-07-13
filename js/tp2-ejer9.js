89d74c9d1066e2d46c76f00bd2a891e85cb8719e
Javier Uncos <javiersrfc30@gmail.com>
2022-07-13 16:59:17 -0300

    se realizo el ejercicio 9 con bucle for

--- js/tp2-ejer9.js
+++ js/tp2-ejer9.js
@@ -29,3 +29,21 @@
 
 
 
+// escribir numero del 1 al 500 
+// tengo que saber que numero son multiplo de 9 o de 4
+
+let numero = 1;
+for(let i = 1;i<=500;i++){
+    document.write(i);
+    if(i % 4 === 0){
+        document.write(' es multiplo de 4')
+    }
+    if(i % 9 === 0){
+        document.write(' es multiplo de 9')
+    }
+    document.write('<br>');
+    if(i % 5 === 0){
+        document.write('<hr>')
+    }
+
+}
