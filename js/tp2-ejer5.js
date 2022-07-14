f59d49bf1bf2a03b57b1ac60479b19bd2ebde36a
Javier Uncos <javiersrfc30@gmail.com>
2022-07-14 15:28:09 -0300

    proceso de solucion del ejercicio 5

--- js/tp2-ejer5.js
+++ js/tp2-ejer5.js
@@ -1,29 +1,95 @@
- 
+let numero = parseInt(prompt('ingressa un numero de dni'))
 
-let numeroDni = parseInt(prompt('ingresa un numero'))
-let letras = 'TRWAGMYFPDXBNJZSQVLCKE'
- 
-
-let divisor = 23;
-
-let resultado = numeroDni % divisor;
+console.log(numero)
 
 
+  do{
 
-if(isNaN(numeroDni)){
-    alert('no es valido')
-    numeroDni=parseInt(prompt("ingresa un numero nuevamente"))
-}else{
-   
-    for(let i = 0; i<letras.length;i++){
-     
-        if(resultado === letras.indexOf(letras[i])){  
-                if(confirm('confirma tus numero')){
+  confirm('nuevo');{
+    break
+  }
+    
+    
+}while(numer>=0 && umero<=99999999){
 
-                }else{
-                    console.log(letras.charAt(i))
-                }
-            }
+    if(numero >=0 && numero <= 99999999){
+        let resultado = numero % 23;
+        switch(resultado){
+            case 0:
+                document.write("T");
+                break;
+            case 1:
+                document.write("R");
+                break;
+            case 2:
+                document.write("W");
+                break;
+            case 3:
+                document.write("A");
+                break;
+            case 4:
+                document.write("G");
+                break;
+            case 5:
+                document.write("M");
+                break;
+            case 6:
+                document.write("Y");
+                break;
+            case 7:
+                document.write("F");
+                break;
+            case 8:
+                document.write("P");
+                break;
+            case 9:
+                document.write("D");
+                break;
+            case 10:
+                document.write("X");
+                break;
+            case 11:
+                document.write("B");
+                break;
+            case 12:
+                document.write("N");
+                break;
+            case 12:
+                document.write("J");
+                break;
+            case 13:
+                document.write("Z");
+                break;
+            case 14:
+                document.write("S");
+                break;
+            case 15:
+                document.write("Q");
+                break;
+            case 16:
+                document.write("V");
+                break;
+            case 17:
+                document.write("H");
+                break;
+            case 18:
+                document.write("L");
+                break;
+            case 19:
+                document.write("C");
+                break;
+            case 20:
+                document.write("K");
+                break;
+            case 21:
+                document.write("E");
+                break;
+                default:
+                    alert('no valido')
+        }
+    }
+    confirm('quieres volver a realizar la operacion');{
+        break
     }
 }
     
@@ -31,49 +97,7 @@ if(isNaN(numeroDni)){
 
 
 
-            
 
-            
-         
-
-          
-
-    
+  
 
 
-
-
-
-
-
-
-
-
-// for(let i = 0; i<letras.length;i++)
-
-// for(let i = 0; i<letras.length;i++){
-        
-//             if(isNaN(numeroDni)){
-//                 alert('no valido')
-//                 numeroDni = parseInt(prompt('ingresa tu numero de dni'));
-               
-               
-                
-//             }else if(isNaN(numeroDni)){
-//                 (resultado === letras.indexOf(letras[i]))
-//                 console.log(letras.charAt(i))
-//             }
-
-//             }
-
-       
-    
-
-
-
-// // if(resultado === 16){
-//     console.log(letras.indexOf("Z"))
-// }
-
-
-    
