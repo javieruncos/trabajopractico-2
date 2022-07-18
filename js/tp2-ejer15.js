d26ed14d25ac34a04afc2bf90dc08bab722b17a6
Javier Uncos <javiersrfc30@gmail.com>
2022-07-18 11:37:28 -0300

    realizamos el ejercicio numero 4 con un ciclo for

--- js/tp2-ejer15.js
+++ js/tp2-ejer15.js
@@ -1,21 +1,16 @@
-
-
-let frase = prompt("ingresa una frase");
-frase = frase.toLowerCase()
-let vocal = ["a","e","i","o","u"];
+let frase = prompt('ingresa una frase');
+let vocales = ["a","e","i","o","u"]
 let contador = 0;
 
-for(let i = 0; i<frase.length;i++){
-    if (vocal.indexOf(frase[i])
-    != -1){
-        contador++
-        console.log(frase);
-        
-    }
+for(let i = 0; i <= frase.length; i++){
+  if(vocales.indexOf(frase[i]) != -1){
+    contador++
+  }
 }
-//     if(frase.charAt(i)=== vocal[0]||frase.charAt(i)=== vocal[1]||frase.charAt(i)=== vocal[3]||frase.charAt(i)=== vocal[4]||frase.charAt(i)=== vocal[5]){
-//         contador++
-//         console.log(contador)
-//     }
-// }
 
+document.write("numero de vocales existentes en la frase: " + contador)
+
+
+
+
+   
--- js/tp2-ejer4.js
+++ js/tp2-ejer4.js
@@ -1,21 +1,86 @@
 
+// let suma = 0;
+// let contador = 0;
+
+
+
+
+  
+
+let suma = 0;
     
-  while(confirm("realiza una suma de dos numero")){
-    let numero1 = parseInt(prompt('ingresa un numero'));
-    let numero2 = parseInt(prompt('ingresa un numero'));
-    let resultado = numero1+numero2;
-    document.write("el resultado es "+resultado+"<br>")
-    
-    if(confirm('realizar una suma')){
-      let numero1 = parseInt(prompt('ingresa un numero'));
-      let numero2 = parseInt(prompt('ingresa un numero'));
-      let resultado = numero1+numero2;
-      document.write("el  resultado es "+resultado+"<br>")
-    }else{
-      break
-    }
+for(let i = 0 ; i <= 6;i++){
+  let numero = parseInt(prompt('ingresa un numero'))
+  
+  if(Number(numero)=== numero){
+    suma = suma + numero;
+
+   
+  }else{
+    document.write(suma)
+    break
   }
 
+  if(Number(numero)!== numero){
+    alert('error')
+  }
+
+ 
+
+
+
+
+  
+
+  
+
+   
+}
+
+
+
+
+
+  
+  
+  
+  
+
+
+   
+    
+
+
+    // else{
+    //   document.write(suma)
+    //   break
+    // }
+      
+   
+  
+
+    
+    
+   
+   
+  
+  
+  
+  
+
+  
+  
+  
+  // contador++
+
+  
+  
+  
+
+  //  console.log(numero)
+  
+
+  
 
 
 
@@ -29,8 +94,26 @@
 
 
 
-
-
+  
+  
+  
+  // let numero = parseInt(prompt('ingresa otro numero'))
+  // do{
+  //   if(numero!=undefined){
+  //     let numero=parseInt(prompt('ingresa un numero'))
+  //     let suma =0;
+  //     suma = suma+numero;
+      
+  //     document.write(suma)
+  //   }
+  //   // let resultado = suma+numero;
+    //  numero =parseInt(prompt('ingresa un numero'))
+   
+    // let numero = parseInt(prompt('ingresa un numero'))
+  
+// }while(!isNaN(numero)){
+  
+// }
 
 
 
