f22e7ba43f176e88b33e4f1f569b9876030a8c56
Javier Uncos <javiersrfc30@gmail.com>
2022-07-18 16:11:47 -0300

    realizamos el ejercicio 6

--- js/tp2-ejer3.js
+++ js/tp2-ejer3.js
@@ -1,15 +1,23 @@
 
   
-  for(let i = 0;i<=100;i++){
+//   for(let i = 0;i<=100;i++){
       
-      if(confirm('confirma tu frase')){
-          let frase = prompt('ingresa una frase'); 
-          i++
-          document.write(frase+"-")
-    }else{
-          break
+//       if(confirm('confirma tu frase')){
+//           let frase = prompt('ingresa una frase'); 
+//           i++
+//           document.write(frase+"-")
+//     }else{
+//           break
 
-      }
-  }
+//       }
+//   }
     
-      
+
+for(let i = 0 ; i <= 20;i++){
+    let frase = prompt('ingresa una frase');
+    
+    if(frase === null){
+        break
+    }
+    document.write(frase+"-")
+}
--- js/tp2-ejer6.js
+++ js/tp2-ejer6.js
@@ -1,11 +1,29 @@
 
 
-let numero1 = prompt("ingresa un numero");
 
 
 
-for (let i = 0;i <= numero1;i++){
-    document.write(numero1.repeat(i)+"<br>")
+let repeticion = 0;
+
+for(let i = 1 ; i <= 30 ; i++){
+    for(repeticion = 0; repeticion < i; repeticion++){
+        document.write(i)
+    }
+
+    document.write("<br>")
 }
+
+
+
+
+
+
+// let numero1 = prompt("ingresa un numero");
+
+
+
+// for (let i = 0;i <= numero1;i++){
+//     document.write(numero1.repeat(i)+"<br>")
+// }
 
 
